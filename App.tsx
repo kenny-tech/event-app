e9290833020d277f0e35c7e8c094af011d182577
kenny-tech <kennyemma2008@gmail.com>
2024-07-03 17:26:58 -0700

    Added expo google font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,39 @@
 import { NavigationContainer } from '@react-navigation/native';
 import Nav from './src/Navigation/Nav';
+import { View } from 'react-native';
+import {
+  useFonts,
+  Montserrat_100Thin,
+  Montserrat_200ExtraLight,
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+  Montserrat_900Black,
+} from "@expo-google-fonts/montserrat";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Nav />
-    </NavigationContainer>
-  );
+  let [fontsLoaded] = useFonts({
+    Montserrat_100Thin,
+    Montserrat_200ExtraLight,
+    Montserrat_300Light,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+    Montserrat_800ExtraBold,
+    Montserrat_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <View></View>;
+  } else {
+    return (
+      <NavigationContainer>
+        <Nav />
+      </NavigationContainer>
+    );
+  }
 }
